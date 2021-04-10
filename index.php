74a08e7209ad0e42149f94019cefde5577771a95
Branislav Lang <64263680+branlicious@users.noreply.github.com>
2021-04-10 22:03:06 +0200

    index

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php echo "hello world"
+<?php
+require __DIR__ . '/vendor/autoload.php';
 
-?>
+$crawler = new Bazos\Crawler();
+
+$items = $crawler->crawlAds('https://auto.bazos.sk/', 5); // 5 means number of pages to parse, default is 1
+
+foreach ($items as $item) {
+    echo $item->getTitle() . "\n";
+}
